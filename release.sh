85cb19896f71e8d1066c2fa5638d78c78e527a15
name <local@github.com>
2023-03-07 08:04:40 +0800

    temp2

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-go install github.com/mitchellh/gox
+go install github.com/mitchellh/gox@latest
 
 mkdir -p release
 
@@ -16,17 +16,17 @@ osarch="!darwin/arm !darwin/386"
 
 echo "Compiling:"
 
-os="windows linux darwin"
-arch="amd64 386 arm arm64 mips mips64 mipsle mips64le"
+os="windows linux"
+arch="amd64"
 pushd cmd/ck-client || exit 1
 CGO_ENABLED=0 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
-CGO_ENABLED=0 GOOS="linux" GOARCH="mips" GOMIPS="softfloat" go build -ldflags "-X main.version=${v}" -o ck-client-linux-mips_softfloat-"${v}"
-CGO_ENABLED=0 GOOS="linux" GOARCH="mipsle" GOMIPS="softfloat" go build -ldflags "-X main.version=${v}" -o ck-client-linux-mipsle_softfloat-"${v}"
+#CGO_ENABLED=0 GOOS="linux" GOARCH="mips" GOMIPS="softfloat" go build -ldflags "-X main.version=${v}" -o ck-client-linux-mips_softfloat-"${v}"
+#CGO_ENABLED=0 GOOS="linux" GOARCH="mipsle" GOMIPS="softfloat" go build -ldflags "-X main.version=${v}" -o ck-client-linux-mipsle_softfloat-"${v}"
 mv ck-client-* ../../release
 popd
 
 os="linux"
-arch="amd64 386 arm arm64"
+arch="amd64"
 pushd cmd/ck-server || exit 1
 CGO_ENABLED=0 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
 mv ck-server-* ../../release
